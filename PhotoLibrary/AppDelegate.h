77fb9cf9624662dbe0d9a862e8722c656aa6cd8d
杨建良 <529447249@qq.com>
2016-06-03 09:10:04 +0800

    新建项目

---
+++ PhotoLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoLibrary
+//
+//  Created by 杨建良 on 16/6/3.
+//  Copyright © 2016年 J.L.Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
